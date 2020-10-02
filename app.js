d75dffb9d49ae4a147e3a75b760fbb4fc4b3aa04
Rahul P <Rahul.P@viacomcontractor.com>
2020-10-02 20:25:04 +0530

    BCB-001 ADD: Basic template for Bokclub backend

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send('<h1>Hello There</h1>')
+});
+
+app.listen(PORT);
