c41146f3ad7377dc1615df4fa7ac0e62325b7b36
Rahul P <rahulpadmaraju16@gmail.com>
2020-10-11 13:20:47 +0530

    BC-001 ADD: auth functionality

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const indexRoutes = require('./routes/main')
+const authRoutes = require('./routes/auth')
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use('/', indexRoutes)
+app.use('/auth', authRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server started at port: ${PORT}`);
